142652655cf20eba10b2e186ab2dc581bce18de7
yassinejebali <yassinejebali94@hotmail.com>
2024-05-30 22:02:04 +0100

    Update user creation form with role selection

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -17,6 +17,26 @@
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email de l'utilisateur</label>
                             <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nom@company.com" required="">
                         </div>
+                        <div class="grid md:grid-cols-2 md:gap-6">
+                            <div>
+                                <label for="role" class="text-sm font-medium text-gray-900 sr-only dark:text-white">Role</label>
+                                <select id="role" name="role"
+                                    class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option value="">Role</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="user">Utilisateur</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label for="role" class="text-sm font-medium text-gray-900 sr-only dark:text-white">Role</label>
+                                <select id="role" name="role"
+                                    class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option value="">local</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="user">Utilisateur</option>
+                                </select>
+                            </div>
+                        </div>
                         <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de Passe</label>
                             <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
